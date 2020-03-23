cc3b8620dba1a68859bf58aafa69d569f2234a95
it11 <it11@jvcfddom001>
2020-03-23 08:32:42 +0700

    adit telah merubah welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Belajar Laravel
                 </div>
 
                 <div class="links">
