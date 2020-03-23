49ff33b760e8a7d65f5c7c5cd25630019bbcb089
idawati5494 <ida21908@gmail.com>
2020-03-23 09:12:20 +0700

    ida merubah welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   PT JVC KENWOOD 2
+                   PT JVC KENWOOD 3
                 </div>
 
                 <!-- <div class="links">
