b1d9a564f0db7aa8b5f48c95afdf1b03a000ecde
it11 <it11@JVC-JEIN.CO.ID>
2020-03-23 08:42:48 +0700

    adit telah merubah welcome jadi jvc kenwood

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Belajar Laravel
+                   PT JVC KENWOOD
                 </div>
 
                 <!-- <div class="links">
