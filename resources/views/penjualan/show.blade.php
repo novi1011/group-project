08454108f7eeb4f62c35692c443aabdef72043e8
novi <novitasarinovi97@gmail.com>
2020-03-25 15:22:42 +0700

    novi telah menambahkan show produk

--- resources/views/penjualan/show.blade.php
+++ resources/views/penjualan/show.blade.php
@@ -2,9 +2,107 @@
 
 @section('content')
 
-<p>{{$produk->Product_name}}</p>
+<div class="container">
+    <div class="row text-center">
+        <div class="col-sm-12">
+            <h3>Keterangan Produk</h3>
+        </div>
+    </div>
+
+    <hr>
+   
+
+    <div class="row text-center">
+        <div class="col-sm-12">
+            <h3><b>Product Name</b></h3>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-sm-4">
+            <hr>
+        </div>
+        <div class="col-sm-4">
+            {{$produk->Product_name}}
+        </div>
+        <div class="col-sm-4">
+            <hr>
+        </div>
+    </div>
+
+    <div class="row text-center">
+        <div class="col-sm-12">
+            <h3><b>Supplier_id</b></h3>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-sm-4">
+            <hr>
+        </div>
+        <div class="col-sm-4">
+            {{$customers->Supplier_name}}
+        </div>
+        <div class="col-sm-4">
+            <hr>
+        </div>
+    </div>
+
+    <div class="row text-center">
+        <div class="col-sm-12">
+            <h3><b>Unit Price</b></h3>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-sm-4">
+            <hr>
+        </div>
+        <div class="col-sm-4">
+            {{$customers->Unit_price}}
+        </div>
+        <div class="col-sm-4">
+            <hr>
+        </div>
+    </div>
+
+    <div class="row text-center">
+        <div class="col-sm-12">
+            <h3><b>Quantity</b></h3>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-sm-4">
+            <hr>
+        </div>
+        <div class="col-sm-4">
+            {{$customers->Quantity}}
+        </div>
+        <div class="col-sm-4">
+            <hr>
+        </div>
+    </div>
+
+    <hr>
+    <div class="col text-center">
+        <div class="col-sm-12">
+         <a class="btn btn-primary" href="{{ route('customer.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Produk Name</h5>
+    <p class="card-text">{{$produk->Product_name}}</p>
+  </div>
+</div>
+
+<!-- <p>{{$produk->Product_name}}</p>
 <p>{{$produk->Supplier_id}}</p>
 <p>{{$produk->Unit_price}}</p>
-<p>{{$produk->Quantity}}</p>
+<p>{{$produk->Quantity}}</p> -->
 
 @endsection
