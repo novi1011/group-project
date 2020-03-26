7de32e95bf5a54b20d87f6e3c9a9f406ba5af1ab
novi1011 <novitasarinovi97@gmail.com>
2020-03-26 09:25:27 +0700

    novi mengedit view show

--- resources/views/penjualan/show.blade.php
+++ resources/views/penjualan/show.blade.php
@@ -2,17 +2,36 @@
 
 @section('content')
     <div class="row text-center">
-        <div class="col-sm-12">
-            <hr>
-                <h3>Keterangan Produk</h3>
-            <hr>
-            <p>{{$produk->Product_name}}</p>
-            <p>{{$produk->Supplier_id}}</p>
-            <p>{{$produk->Unit_price}}</p>
-            <p>{{$produk->Quantity}}</p>
-            <hr>
-            <a href="{{ route('produk.index') }}" class="btn btn-danger btn-sm" style="width:150px;">Back</a>
+        <div class="container-fluid">
+             <div class="row">
+                <div class="col-sm-6 col-sm-offset-2">
+
+        <h3>Keterangan Produk</h3><b>
+        <br>
+        
+        <table class="table table-bordered">
+  <thead class="thead-light">
+    <tr class= "text-center">
+      <th scope="col">Product Name</th>
+      <th scope="col">Supplier ID</th>
+      <th scope="col">Unit Price</th>
+      <th scope="col">Quantity</th>
+    </tr>
+    
+  </thead>
+  <tbody>
+    <tr>
+      <td>{{$produk->Product_name}}</td>
+      <td>{{$produk->Supplier_id}}</td>
+      <td>{{$produk->Unit_price}}</td>
+      <td>{{$produk->Quantity}}</td>
+      </tr>
+  </tbody>
+</table>
+    <br>
+    <a href="{{ route('produk.index') }}" class="btn btn-danger btn-sm" style="width:150px;">Back</a>
+        
         </div>
     </div>
-
+       
 @endsection
