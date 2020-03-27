8a25142c74b3bcad652c501e3a6b2e86ba840b18
it11 <it11@JVC-JEIN.CO.ID>
2020-03-27 12:24:41 +0700

    adit memperbaharui migration

--- database/migrations/2020_03_24_070100_create_orders_table.php
+++ database/migrations/2020_03_24_070100_create_orders_table.php
@@ -14,6 +14,7 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
+            $table->string('nama_customer');
             $table->increments('id');
             $table->string('Product_name');
             $table->decimal('Unit_price');
