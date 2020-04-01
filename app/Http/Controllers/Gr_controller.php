de2ed17ab6d8fc468b37bdb54857fd66b46cb152
idawati5494 <ida21908@gmail.com>
2020-04-02 06:13:54 +0700

    ida menambah function pada gr_controler

--- app/Http/Controllers/Gr_controller.php
+++ app/Http/Controllers/Gr_controller.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use\App\Models\Goods_receipt;
+use App\Models\Purchase_order;
+use App\Models\Purchase_order_line;
 
 class Gr_controller extends Controller
 {
@@ -13,5 +15,11 @@ class Gr_controller extends Controller
         $data = Goods_receipt::orderBy('created_at','desc')->get();
 
         return view('gr.index',compact('title','data'));
+    }
+    public function detail($id){
+        $dt = Goods_receipt::find($id);
+        $title = "Detail GR  $dt->document_no";
+
+        return view('gr.detail',compact('title','dt'));
     }
 }
