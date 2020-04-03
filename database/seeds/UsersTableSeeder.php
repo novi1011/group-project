9499e1236323c773da516fbea23f4cfd460299a3
novi <novitasarinovi97@gmail.com>
2020-04-03 14:42:38 +0700

    novi telah mengedit pada app

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,26 +11,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-            'name' => 'Novitasari',
-            'email' => 'novitasarinovi97@gmail.com',
-            'password' => bcrypt('secret'),
-            'role_id' => 1
-        ]);
+    //     App\User::create([
+    //         'name' => 'Novitasari',
+    //         'email' => 'novitasarinovi97@gmail.com',
+    //         'password' => bcrypt('secret'),
+    //         'role_id' => 1
+    //     ]);
 
-        App\User::create([
-            'name' => 'Idawati',
-            'email' => 'idawati5494@gmail.com',
-            'password' => bcrypt('secret'),
-            'role_id' => 2
-        ]);
+    //     App\User::create([
+    //         'name' => 'Idawati',
+    //         'email' => 'idawati5494@gmail.com',
+    //         'password' => bcrypt('secret'),
+    //         'role_id' => 2
+    //     ]);
         
-        App\User::create([
-            'name' => 'Aditya',
-            'email' => 'aditya22@gmail.com',
-            'password' => bcrypt('secret'),
-            'role_id' => 3
-        ]);
+    //     App\User::create([
+    //         'name' => 'Aditya',
+    //         'email' => 'aditya22@gmail.com',
+    //         'password' => bcrypt('secret'),
+    //         'role_id' => 3
+
+    //     ]);
 
     }
 }
