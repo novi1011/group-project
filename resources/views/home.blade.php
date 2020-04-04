7a4aed8b60a8a47e3a453bd378aa2affd61641a1
idawati5494 <ida21908@gmail.com>
2020-04-04 08:43:22 +0700

    ida merubah home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,75 @@
 @extends('layouts.master')
+
+@section('content')
+<div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>{{ $tot_produk }}</h3>
+
+              <p>Total produk</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>{{ $tot_po }}<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Total PO</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>{{ $tot_supplier }}</h3>
+
+              <p>Total Supplier</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>{{ $tot_gr }}</h3>
+
+              <p>Good Receipt</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <!-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+<hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr>
+<!-- <div class="row"> -->
+    <!-- <div class="col-12"> -->
+       
+            <strong>Copyright © 2020 <a>Group_Project</a></strong> All rights
+            reserved.
+      
+    <!-- </div>
+</div> -->
+@endsection
