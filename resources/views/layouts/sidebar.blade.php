f570cf9439e1c4db055cebe2b5e31bf07eeb581e
meeting20 <meeting20@JVC-JEIN.CO.ID>
2020-04-21 11:53:13 +0700

    adit memperbaharui side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,24 +10,24 @@
 
         
         @if(auth()->user()->role == 'supplier')
-        <li class="menu-sidebar"><a href="{{ url('/produk') }}"><span class="fa fa-table"></span> Produk</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/produk') }}"><span class="fa fa-table"></span> Add Product Data</span></a></li>
         @endif
 
         @if(auth()->user()->role == 'customer')
-        <li class="menu-sidebar"><a href="{{ url('/po/') }}"><span class="fa fa-shopping-cart"></span> Pesan produk</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/po/') }}"><span class="fa fa-shopping-cart"></span> Product Orders</span></a></li>
         @endif
 
         @if(auth()->user()->role == 'admin')
-        <li class="menu-sidebar"><a href="{{ url('/supplier') }}"><span class="fa fa-meh-o"></span> Supplier</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/supplier') }}"><span class="fa fa-user-plus"></span> Add Supplier Data</span></a></li>
         @endif
         @if(auth()->user()->role == 'admin')
-        <li class="menu-sidebar"><a href="{{ url('/supplier/index') }}"><span class="fa fa-meh-o"></span> Lihat List Supplier</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/supplier/index') }}"><span class="fa fa-meh-o"></span> Supplier List</span></a></li>
         @endif
         @if(auth()->user()->role == 'admin')
-        <li class="menu-sidebar"><a href="{{ url('/produk/index') }}"><span class="fa fa-table"></span> Lihat List Produk</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/produk/index') }}"><span class="fa fa-table"></span> Product List</span></a></li>
         @endif
         @if(auth()->user()->role == 'admin')
-        <li class="menu-sidebar"><a href="{{ url('/po/index') }}"><span class="fa fa-shopping-cart"></span> Lihat List Pesan produk</span></a></li>
+        <li class="menu-sidebar"><a href="{{ url('/po/index') }}"><span class="fa fa-shopping-cart"></span> Order List</span></a></li>
         @endif
         @if(auth()->user()->role == 'admin')
         <li class="menu-sidebar"><a href="{{ url('/gr/') }}"><span class="fa fa-check-square-o"></span> Goods Receipt</span></a></li>
